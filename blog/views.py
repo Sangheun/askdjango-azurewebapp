2dd563d4b023da70568bbf6cda57b2bb86a0c060
Sangheun Lee <aisiterugh@snu.ac.kr>
2016-04-20 15:49:19 +0900

    5th

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+
 def post_list(request):
     post_list = Post.objects.all()
     return render(request, 'blog/post_list.html', {
@@ -9,6 +10,6 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
-    return render(request, 'blog/post_detail.gtml', {
+    return render(request, 'blog/post_detail.html', {
         'post': post,
         })
